ea5bb086dbe764fed7d2d160c412897f1828b274
Aaditya Ranjan <ranjanaadi50@gmail.com>
2026-02-04 15:34:47 +0530

    [Aaditya]: Implement UC2: Render OOPS banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,27 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This program extends UC1 by displaying the word "OOPS"
+ * in a large ASCII banner format using asterisks (*) and spaces.
+ * Each line of the banner is printed using individual
+ * System.out.println() statements with string concatenation.
+ *
+ * Concepts Used:
+ * - ASCII Art
+ * - String Concatenation
+ * - Multiple Print Statements
+ */
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("    ***    " + "    ***    " + " *******   " + "    ******* ");
+        System.out.println("  **   **  " + "  **   **  " + " **     ** " + "  **        ");
+        System.out.println(" **     ** " + " **     ** " + " **     ** " + "    **      ");
+        System.out.println(" **     ** " + " **     ** " + " *******   " + "      **    ");
+        System.out.println(" **     ** " + " **     ** " + " **        " + "         ** ");
+        System.out.println("  **   **  " + "  **   **  " + " **        " + "         ** ");
+        System.out.println("    ***    " + "    ***    " + " **        " + "  *******   ");
     }
 }
